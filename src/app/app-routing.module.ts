f8f35d6ea2f86b73c64954d39fa93cdf8c6a8ec1
Fahrul Arya Sugita <faryasugita@gmail.com>
2020-06-11 09:24:39 +0700

    istimiwir

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PlayersComponent } from './players/players.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",redirectTo:"/players",pathMatch:"full"},
+  {path:"players",component:PlayersComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlayersComponent } from './players/players.component';
+import { AgePipe } from './age.pipe';
+import { FilterPipe } from './filter.pipe';
+import { ScorePipe } from './score.pipe';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlayersComponent,
+    AgePipe,
+    FilterPipe,
+    ScorePipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
